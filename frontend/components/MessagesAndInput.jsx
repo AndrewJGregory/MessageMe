6c460759e79e0df5483568c32a203f8b647f9f5c
Andrew Gregory <andrewjgregoryajg@gmail.com>
2018-03-16 20:02:53 -0400

    style user search results, messages

--- frontend/components/MessagesAndInput.jsx
+++ frontend/components/MessagesAndInput.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const MessagesAndInput = () => {
-  return <section>Messages and input go here</section>;
+  return <section className="messages">Messages and input go here</section>;
 };
 
 export default MessagesAndInput;
--- frontend/components/Search.jsx
+++ frontend/components/Search.jsx
@@ -3,8 +3,10 @@ import React from "react";
 export default class Search extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { query: "" };
+    this.state = { query: "", textAlignment: "" };
     this.updateInput = this.updateInput.bind(this);
+    this.leftAlignText = this.leftAlignText.bind(this);
+    this.centerAlignText = this.centerAlignText.bind(this);
   }
 
   updateInput(e) {
@@ -15,20 +17,41 @@ export default class Search extends React.Component {
     });
   }
 
+  leftAlignText(e) {
+    e.preventDefault();
+    this.setState({ textAlignment: "left-text-align" });
+  }
+
+  centerAlignText(e) {
+    e.preventDefault();
+    this.setState({ textAlignment: "" });
+  }
+
   render() {
     const users = this.props.userResults.map(user => {
-      return <li key={user.id}>{user.username}</li>;
+      return (
+        <li key={user.id} className="user-search-result clickable">
+          {user.username}
+        </li>
+      );
     });
 
     return (
-      <div>
-        <input
-          type="text"
-          onChange={this.updateInput}
-          value={this.state.query}
-          placeholder="Search MessageMe"
-        />
-        <ul>{users}</ul>
+      <div className="search">
+        <div className="user-search-input-container">
+          <i className="fa fa-search" />
+          <input
+            type="text"
+            onChange={this.updateInput}
+            value={this.state.query}
+            placeholder="Search MessageMe"
+            className="user-search-input"
+            id={`${this.state.textAlignment}`}
+            onFocus={this.leftAlignText}
+            onBlur={this.centerAlignText}
+          />
+        </div>
+        <ul className="user-search-results">{users}</ul>
       </div>
     );
   }
