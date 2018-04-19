fdffb0356ea64739daddf0b78b838ac3e9ff3a31
Andrew Gregory <andrewjgregoryajg@gmail.com>
2018-04-18 22:32:40 -0400

    remove duplicate code, use helper function

--- frontend/components/ChatWebSocketContainer.js
+++ frontend/components/ChatWebSocketContainer.js
@@ -2,19 +2,7 @@ import { connect } from "react-redux";
 import ChatWebSocket from "./ChatWebSocket";
 import { withRouter } from "react-router-dom";
 import { receiveMessage } from "../actions/message";
-
-const findChatId = (state, userId, currentUserId) => {
-  let chatId = 0;
-  Object.values(state.entities.chats).forEach(chat => {
-    if (
-      (chat.user_id_one === userId && chat.user_id_two === currentUserId) ||
-      (chat.user_id_one === currentUserId && chat.user_id_two === userId)
-    ) {
-      chatId = chat.id;
-    }
-  });
-  return chatId;
-};
+import { findChatId } from "../util/chat";
 
 const mapStateToProps = (state, ownProps) => {
   const userId = parseInt(ownProps.match.params.userId);
