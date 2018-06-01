1eabf1e5a4c6bccdd6cbc36f9cf6363364db084c
Andrew Gregory <andrewjgregoryajg@gmail.com>
2018-06-01 13:47:04 -0400

    send message to a different session's state (finally!)

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -2,8 +2,17 @@ class MessageBroadcastJob < ApplicationJob
   queue_as :default
 
   def perform(message, chat_id)
-    @chat = Chat.find_by(id: chat_id )
+    original_chat = Chat.find_by(id: chat_id)
+    chat = message.chat
+    ids = [chat.user_id_one, chat.user_id_two]
+    ids.reject! { |id| id === message.user_id }
+    # stream new message to OTHER user's chat with themself
+    self_chat = Chat.find_by(user_id_one: ids.first, user_id_two: ids.first)
     normalized_message = { message.id => message }
-    ChatChannel.broadcast_to(@chat, { message: normalized_message })
+    # other user needs to have the chat with themselves. 
+    # so create chat with the users self after creation in model (instead of on click in front end)
+    # 
+    ChatChannel.broadcast_to(self_chat, { message: normalized_message })
+    ChatChannel.broadcast_to(original_chat, { message: normalized_message })
   end
 end
