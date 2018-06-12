6f560cf3d9f72a92533757af3cf620fe40ad8025
Andrew Gregory <andrewjgregoryajg@gmail.com>
2018-06-12 11:56:11 -0400

    make login case sensitive

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -2,7 +2,7 @@ class Api::SessionsController < ApplicationController
   def create
 
     @user = User.find_by_credentials(
-      params[:user][:username].downcase,
+      params[:user][:username],
       params[:user][:password]
     )
 
