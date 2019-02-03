d29bc11b6c057f7baf851a8be673e71224948a20
Andrew Gregory <andrewjgregoryajg@gmail.com>
2019-02-03 18:52:25 -0500

    add cog icon to sidebar header

--- frontend/components/Sidebar.jsx
+++ frontend/components/Sidebar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import SearchContainer from "./SearchContainer";
+import SidebarHeader from "./SidebarHeader";
 
 const Sidebar = () => {
   return (
     <aside className="sidebar">
-      <div className="sidebar__header center truncate">MessageMe</div>
+      <SidebarHeader />
       <SearchContainer />
     </aside>
   );
---
+++ frontend/components/SidebarHeader.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+
+import PropTypes from "prop-types";
+
+export default () => {
+  return (
+    <div className="sidebar-header-container">
+      <i className="fa fa-cog medium" />
+      <div className="sidebar__header">MessageMe</div>
+      <i class="fa fa-edit" />
+    </div>
+  );
+};
